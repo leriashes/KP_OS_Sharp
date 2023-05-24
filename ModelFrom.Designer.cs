0c3553ad334c4923d90c99d618099f77bfd3449a
leriashes <valeriya-shevchenko-2002@mail.ru>
2023-05-24 13:42:42 +0700

    Добавьте файлы проекта.

---
+++ ModelFrom.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace KP_OS_Sharp
+{
+	partial class ModelFrom
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.groupBox9 = new System.Windows.Forms.GroupBox();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.groupBox5 = new System.Windows.Forms.GroupBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.groupBox6 = new System.Windows.Forms.GroupBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.groupBox7 = new System.Windows.Forms.GroupBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.groupBox8 = new System.Windows.Forms.GroupBox();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.groupBox4.SuspendLayout();
+			this.groupBox9.SuspendLayout();
+			this.groupBox5.SuspendLayout();
+			this.groupBox6.SuspendLayout();
+			this.groupBox7.SuspendLayout();
+			this.groupBox8.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.BackColor = System.Drawing.Color.Honeydew;
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Location = new System.Drawing.Point(7, 6);
+			this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+			this.groupBox1.Size = new System.Drawing.Size(218, 325);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Процесс 1";
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+			this.textBox1.Location = new System.Drawing.Point(5, 18);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox1.Size = new System.Drawing.Size(208, 302);
+			this.textBox1.TabIndex = 0;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.BackColor = System.Drawing.Color.Honeydew;
+			this.groupBox2.Controls.Add(this.textBox2);
+			this.groupBox2.Location = new System.Drawing.Point(228, 6);
+			this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+			this.groupBox2.Size = new System.Drawing.Size(218, 325);
+			this.groupBox2.TabIndex = 2;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Процесс 2";
+			this.groupBox2.Visible = false;
+			// 
+			// textBox2
+			// 
+			this.textBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.textBox2.Cursor = System.Windows.Forms.Cursors.Default;
+			this.textBox2.Location = new System.Drawing.Point(5, 18);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox2.Size = new System.Drawing.Size(208, 302);
+			this.textBox2.TabIndex = 1;
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.BackColor = System.Drawing.Color.Honeydew;
+			this.groupBox3.Controls.Add(this.textBox3);
+			this.groupBox3.Location = new System.Drawing.Point(450, 6);
+			this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
+			this.groupBox3.Size = new System.Drawing.Size(218, 325);
+			this.groupBox3.TabIndex = 3;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Процесс 3";
+			this.groupBox3.Visible = false;
+			// 
+			// textBox3
+			// 
+			this.textBox3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.textBox3.Cursor = System.Windows.Forms.Cursors.Default;
+			this.textBox3.Location = new System.Drawing.Point(5, 18);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.ReadOnly = true;
+			this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox3.Size = new System.Drawing.Size(208, 302);
+			this.textBox3.TabIndex = 1;
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.BackColor = System.Drawing.Color.Honeydew;
+			this.groupBox4.Controls.Add(this.textBox4);
+			this.groupBox4.Location = new System.Drawing.Point(672, 6);
+			this.groupBox4.Margin = new System.Windows.Forms.Padding(2);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Padding = new System.Windows.Forms.Padding(2);
+			this.groupBox4.Size = new System.Drawing.Size(218, 325);
+			this.groupBox4.TabIndex = 4;
+			this.groupBox4.TabStop = false;
+			this.groupBox4.Text = "Процесс 4";
+			this.groupBox4.Visible = false;
+			// 
+			// textBox4
+			// 
+			this.textBox4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.textBox4.Cursor = System.Windows.Forms.Cursors.Default;
+			this.textBox4.Location = new System.Drawing.Point(5, 18);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ReadOnly = true;
+			this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox4.Size = new System.Drawing.Size(208, 302);
+			this.textBox4.TabIndex = 1;
+			// 
+			// groupBox9
+			// 
+			this.groupBox9.Controls.Add(this.textBox9);
+			this.groupBox9.Location = new System.Drawing.Point(898, 6);
+			this.groupBox9.Margin = new System.Windows.Forms.Padding(2);
+			this.groupBox9.Name = "groupBox9";
+			this.groupBox9.Padding = new System.Windows.Forms.Padding(2);
+			this.groupBox9.Size = new System.Drawing.Size(240, 654);
+			this.groupBox9.TabIndex = 5;
+			this.groupBox9.TabStop = false;
+			this.groupBox9.Text = "Операционная система";
+			// 
+			// textBox9
+			// 
+			this.textBox9.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.textBox9.Cursor = System.Windows.Forms.Cursors.Default;
+			this.textBox9.Location = new System.Drawing.Point(5, 18);
+			this.textBox9.Multiline = true;
+			this.textBox9.Name = "textBox9";
+			this.textBox9.ReadOnly = true;
+			this.textBox9.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox9.Size = new System.Drawing.Size(230, 631);
+			this.textBox9.TabIndex = 1;
+			// 
+			// groupBox5
+			// 
+			this.groupBox5.BackColor = System.Drawing.Color.Honeydew;
+			this.groupBox5.Controls.Add(this.textBox5);
+			this.groupBox5.Location = new System.Drawing.Point(7, 335);
+			this.groupBox5.Margin = new System.Windows.Forms.Padding(2);
+			this.groupBox5.Name = "groupBox5";
+			this.groupBox5.Padding = new System.Windows.Forms.Padding(2);
+			this.groupBox5.Size = new System.Drawing.Size(218, 325);
+			this.groupBox5.TabIndex = 6;
+			this.groupBox5.TabStop = false;
+			this.groupBox5.Text = "Процесс 5";
+			this.groupBox5.Visible = false;
+			// 
+			// textBox5
+			// 
+			this.textBox5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.textBox5.Cursor = System.Windows.Forms.Cursors.Default;
+			this.textBox5.Location = new System.Drawing.Point(5, 18);
+			this.textBox5.Multiline = true;
+			this.textBox5.Name = "textBox5";
+			this.textBox5.ReadOnly = true;
+			this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox5.Size = new System.Drawing.Size(208, 302);
+			this.textBox5.TabIndex = 1;
+			// 
+			// groupBox6
+			// 
+			this.groupBox6.BackColor = System.Drawing.Color.Honeydew;
+			this.groupBox6.Controls.Add(this.textBox6);
+			this.groupBox6.Location = new System.Drawing.Point(228, 335);
+			this.groupBox6.Margin = new System.Windows.Forms.Padding(2);
+			this.groupBox6.Name = "groupBox6";
+			this.groupBox6.Padding = new System.Windows.Forms.Padding(2);
+			this.groupBox6.Size = new System.Drawing.Size(218, 325);
+			this.groupBox6.TabIndex = 7;
+			this.groupBox6.TabStop = false;
+			this.groupBox6.Text = "Процесс 6";
+			this.groupBox6.Visible = false;
+			// 
+			// textBox6
+			// 
+			this.textBox6.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.textBox6.Cursor = System.Windows.Forms.Cursors.Default;
+			this.textBox6.Location = new System.Drawing.Point(5, 18);
+			this.textBox6.Multiline = true;
+			this.textBox6.Name = "textBox6";
+			this.textBox6.ReadOnly = true;
+			this.textBox6.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox6.Size = new System.Drawing.Size(208, 302);
+			this.textBox6.TabIndex = 1;
+			// 
+			// groupBox7
+			// 
+			this.groupBox7.BackColor = System.Drawing.Color.Honeydew;
+			this.groupBox7.Controls.Add(this.textBox7);
+			this.groupBox7.Location = new System.Drawing.Point(450, 335);
+			this.groupBox7.Margin = new System.Windows.Forms.Padding(2);
+			this.groupBox7.Name = "groupBox7";
+			this.groupBox7.Padding = new System.Windows.Forms.Padding(2);
+			this.groupBox7.Size = new System.Drawing.Size(218, 325);
+			this.groupBox7.TabIndex = 8;
+			this.groupBox7.TabStop = false;
+			this.groupBox7.Text = "Процесс 7";
+			this.groupBox7.Visible = false;
+			// 
+			// textBox7
+			// 
+			this.textBox7.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.textBox7.Cursor = System.Windows.Forms.Cursors.Default;
+			this.textBox7.Location = new System.Drawing.Point(5, 18);
+			this.textBox7.Multiline = true;
+			this.textBox7.Name = "textBox7";
+			this.textBox7.ReadOnly = true;
+			this.textBox7.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox7.Size = new System.Drawing.Size(208, 302);
+			this.textBox7.TabIndex = 1;
+			// 
+			// groupBox8
+			// 
+			this.groupBox8.BackColor = System.Drawing.Color.Honeydew;
+			this.groupBox8.Controls.Add(this.textBox8);
+			this.groupBox8.Location = new System.Drawing.Point(672, 335);
+			this.groupBox8.Margin = new System.Windows.Forms.Padding(2);
+			this.groupBox8.Name = "groupBox8";
+			this.groupBox8.Padding = new System.Windows.Forms.Padding(2);
+			this.groupBox8.Size = new System.Drawing.Size(218, 325);
+			this.groupBox8.TabIndex = 9;
+			this.groupBox8.TabStop = false;
+			this.groupBox8.Text = "Процесс 8";
+			this.groupBox8.Visible = false;
+			// 
+			// textBox8
+			// 
+			this.textBox8.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.textBox8.Cursor = System.Windows.Forms.Cursors.Default;
+			this.textBox8.Location = new System.Drawing.Point(5, 18);
+			this.textBox8.Multiline = true;
+			this.textBox8.Name = "textBox8";
+			this.textBox8.ReadOnly = true;
+			this.textBox8.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox8.Size = new System.Drawing.Size(208, 302);
+			this.textBox8.TabIndex = 1;
+			// 
+			// ModelFrom
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+			this.ClientSize = new System.Drawing.Size(1149, 673);
+			this.Controls.Add(this.groupBox8);
+			this.Controls.Add(this.groupBox7);
+			this.Controls.Add(this.groupBox6);
+			this.Controls.Add(this.groupBox5);
+			this.Controls.Add(this.groupBox9);
+			this.Controls.Add(this.groupBox4);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(1165, 711);
+			this.MinimumSize = new System.Drawing.Size(1165, 711);
+			this.Name = "ModelFrom";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.Text = "ModelFrom";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.groupBox4.ResumeLayout(false);
+			this.groupBox4.PerformLayout();
+			this.groupBox9.ResumeLayout(false);
+			this.groupBox9.PerformLayout();
+			this.groupBox5.ResumeLayout(false);
+			this.groupBox5.PerformLayout();
+			this.groupBox6.ResumeLayout(false);
+			this.groupBox6.PerformLayout();
+			this.groupBox7.ResumeLayout(false);
+			this.groupBox7.PerformLayout();
+			this.groupBox8.ResumeLayout(false);
+			this.groupBox8.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.GroupBox groupBox9;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.GroupBox groupBox5;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.GroupBox groupBox6;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.GroupBox groupBox7;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.GroupBox groupBox8;
+		private System.Windows.Forms.TextBox textBox8;
+	}
+}
